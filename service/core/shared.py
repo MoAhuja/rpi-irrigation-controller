039dfe3764b02b7d800b56a555d223dd13f5c805
Mohit Ahuja <mohit.ahuja@rbc.com>
2018-07-10 08:14:30 -0400

    Cleanup and updating of TODO list

--- service/core/shared.py
+++ service/core/shared.py
@@ -1,7 +1,5 @@
-# from service.core.event_publisher import EventPublisher
-# from service.engine import Engine
+
 from threading import Lock
-# from service.system.settings_manager import SettingsManager
 from service.utilities.logger import Logger
 
 logger = Logger()
