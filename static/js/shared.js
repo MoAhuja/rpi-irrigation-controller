25d7fc6fb025ebfe0234fc5d6b2e0000a1f7e6bb
Mohit Ahuja <mohit.ahuja@rbc.com>
2019-04-22 08:36:05 -0400

    Created a shared class to display web service errors as alerts

--- static/js/shared.js
+++ static/js/shared.js
@@ -11,3 +11,19 @@ function displayAlert( type, message)
 
     $("#alerts_container").append(body);
 }
+
+function displayAlertFromXHR( type, xhr)
+{
+    console.log("Displaying alert - " + type);
+
+    var message = xhr.responseJSON.error + " - " + xhr.responseJSON.field
+
+    var body = `<div class="alert alert-${type} alert-dismissible">
+        ${message}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>`
+
+    $("#alerts_container").append(body);
+}
