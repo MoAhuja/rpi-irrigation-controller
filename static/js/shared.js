ad99bf9381b3c8450c0b434bb428b1e30f335906
Mohit Ahuja <mohit.ahuja@rbc.com>
2019-05-07 18:19:39 -0400

    Moved the page navigation into the shared js file

--- static/js/shared.js
+++ static/js/shared.js
@@ -1,3 +1,45 @@
+currentlyShowingPanel = ""
+
+$(document).ready(function()
+{
+    currentlyShowingPanel = $("#content_dashboard")
+    //Indicate the currently showing panel as the dashboard
+    
+
+    // Hide/show content by default
+    $(function(){
+        // Show this pane to start
+        $("#content_dashboard").show();
+
+        // Hide these ones to start
+        $("#content_manager").hide()
+        
+        $("#content_logs").hide()
+
+        $("#content_settings").hide();
+    });
+
+    $(".main_nav li").click(function() {
+        id = ($(this).attr('id'))
+        
+        //Find the class that's currently showing and toggle it off
+        // $("div.show").hide()
+        $(currentlyShowingPanel).hide()
+
+        //Find the panel and toggle teh show class
+        panelID = "#" + id.replace("btn", "content")
+        // $(panelID).toggleClass("show")
+        $(panelID).show("puff")
+        currentlyShowingPanel = panelID
+
+    });
+
+});
+
+
+
+
+
 function displayAlert( type, message)
 {
     console.log("Displaying alert - " + type);
