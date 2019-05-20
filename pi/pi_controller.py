e0ab2edfa7153a7a9be794b5e5046f4f300ea449
Mo Ahuja <mo.ahuja@gmail.com>
2019-05-20 18:32:44 +0000

    Modified PIController to enable control of RPI. Needs more work.

--- pi/pi_controller.py
+++ pi/pi_controller.py
@@ -1,3 +1,4 @@
+import RPi.GPIO as GPIO
 
 # This class manipulates the PINS on the raspberry pi
 class PIController():
@@ -5,16 +6,29 @@ class PIController():
     def initializePINS(self):
         # Fetch all the required pins from the DB
         # TODO: Initialize all pin to BCD & Pull them down or whatever
+        
         a=0
         # Initialize all the pins to BCD
+        GPIO.setmode(GPIO.BCM);
+        
+        #chan_list = [11,12]    # add as many channels as you want!
+                       # you can tuples instead i.e.:
+                       #   chan_list = (11,12)
+        #GPIO.setup(chan_list, GPIO.OUT)
+        
         # Set all the pins to their closed state/no electricity flowing (valve = off)
+        # Loop through all the pins and set them as output pins
+        #GPIO.setup(1, GPIO.OUT, initial=GPIO.LOW)
 
     def __init__(self):
         # TODO: Initialize all the pins
         self.initializePINS()
     
     def activatePIN(self, pin):
+        #Add check to make sure PIN is numeric and one of the initialized pins
+        GPIO.output(pin, GPIO.HIGH)
         return True
 
     def deactivatePIN(self, pin):
+        GPIO.output(pin, GPIO.LOW)
         return True
