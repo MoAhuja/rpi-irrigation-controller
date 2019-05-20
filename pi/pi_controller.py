2bc887ca3f0b969f2a98bd417d06c8802f1b585b
Mo <mo.ahuja@gmail.com>
2019-05-20 16:00:50 -0400

    Updated initialization of pi controller for testing

--- pi/pi_controller.py
+++ pi/pi_controller.py
@@ -7,14 +7,15 @@ class PIController():
         # Fetch all the required pins from the DB
         # TODO: Initialize all pin to BCD & Pull them down or whatever
         
-        a=0
         # Initialize all the pins to BCD
         GPIO.setmode(GPIO.BCM);
+
+
         
-        #chan_list = [11,12]    # add as many channels as you want!
+        chan_list = [23,4]    # add as many channels as you want!
                        # you can tuples instead i.e.:
                        #   chan_list = (11,12)
-        #GPIO.setup(chan_list, GPIO.OUT)
+        GPIO.setup(chan_list, GPIO.OUT, initial=GPIO.HIGH)
         
         # Set all the pins to their closed state/no electricity flowing (valve = off)
         # Loop through all the pins and set them as output pins
@@ -26,9 +27,9 @@ class PIController():
     
     def activatePIN(self, pin):
         #Add check to make sure PIN is numeric and one of the initialized pins
-        GPIO.output(pin, GPIO.HIGH)
+        GPIO.output(pin, GPIO.LOW)
         return True
 
     def deactivatePIN(self, pin):
-        GPIO.output(pin, GPIO.LOW)
+        GPIO.output(pin, GPIO.HIGH)
         return True
