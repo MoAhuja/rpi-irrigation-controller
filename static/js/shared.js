9f61fcdec867d4d6480d8ac2fd596842fc5e7d84
Mohit Ahuja <mohit.ahuja@rbc.com>
2019-05-29 09:25:37 -0400

    Cleaning up unused files and classes

--- static/js/shared.js
+++ static/js/shared.js
@@ -111,22 +111,6 @@ function displayAlertInContainer(container, type, message)
     container.append(body);
 }
 
-function displayAlertFromXHR( type, xhr)
-{
-    console.log("Displaying alert - " + type);
-
-    var message = xhr.responseJSON.error + " - " + xhr.responseJSON.field
-
-    var body = `<div class="alert alert-${type} alert-dismissible">
-        ${message}
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>`
-
-    $("#alerts_container").append(body);
-}
-
 function displayAlertInContainerFromXHR(container, type, xhr)
 {
     console.log("Displaying alert - " + type);
